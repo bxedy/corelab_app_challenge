efd955bca86c731db70360f821898e680ef3b98d
bxedy <joaov.martins2002@gmail.com>
2023-08-24 21:35:04 -0300

    Refatoração

--- lib/modules/home/presentation/pages/home_page.dart
+++ lib/modules/home/presentation/pages/home_page.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 import '../../../../core/ui/components/app_bottom_navigation_bar.dart';
-import '../controllers/home_controller.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  final IHomeController controller = Modular.get();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        bottomNavigationBar: AppBottomNavigation(
-          currentPage: AvailablePages.home,
-        ),
-        body: Center(),
-      );
+  Widget build(BuildContext context) {
+//    final IHomeController controller = Modular.get();
+    return const Scaffold(
+      bottomNavigationBar: AppBottomNavigation(
+        currentPage: AvailablePages.home,
+      ),
+      body: SafeArea(child: Center()),
+    );
+  }
 }
