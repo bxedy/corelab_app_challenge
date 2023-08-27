62b2d4a4bd9ced3559e7c217215482f13e7f65ee
bxedy <joaov.martins2002@gmail.com>
2023-08-27 19:16:41 -0300

    Organização imports

--- lib/modules/home/presentation/components/search_app_bar.dart
+++ lib/modules/home/presentation/components/search_app_bar.dart
@@ -1,5 +1,5 @@
-import 'package:corelab_app_challenge/core/ui/theme/app_colors.dart';
-import 'package:corelab_app_challenge/core/ui/theme/app_text_styles.dart';
+import '../../../../core/ui/theme/app_colors.dart';
+import '../../../../core/ui/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
